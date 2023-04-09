3d5721c109b9c55c05bbc169105e326a44cfa0ce
Juhyeok0202 <101847711+Juhyeok0202@users.noreply.github.com>
2023-04-09 20:25:39 +0900

    [level 2] Title: 괄호 변환, Time: 8.65 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.java
@@ -1,9 +1,62 @@
+import java.util.Stack;
+
 class Solution {
+     public static boolean isCorrect(String p) {
+        Stack<Character> stk = new Stack<>();
+        for (int i = 0; i < p.length(); ++i) {
+            char c = p.charAt(i);
+
+            if(c=='(') stk.add(c);
+            else {
+                if(stk.isEmpty())
+                    return false;
+                else stk.pop();
+            }
+        }
+        return stk.isEmpty();
+    }
+    
+    private String dfs(String p) {
+        if(p.length()==0)
+            return "";
+
+        String u = "";
+        String v = "";
+
+        int open =0, close=0;
+        
+        for (int i = 0; i < p.length(); ++i) {
+            char c = p.charAt(i);
+            if(c=='(')
+                open++;
+            else
+                close++;
+            if((open != 0 && close != 0) && open == close){
+                u = p.substring(0, i + 1);
+                if(i!=p.length()-1)
+                    v=p.substring(i+1,p.length());
+            }
+            break;
+        }
+
+        if(!isCorrect(u)){
+            String temp = "("+dfs(v);
+            temp += ")";
+            u=u.substring(1,p.length()-1);
+            u=u.replace("(",".");
+            u=u.replace(")","(");
+            u=u.replace(".", ")");
+            temp+=u;
+            return temp;
+        }
+        else{
+            return u+dfs(v);
+        }
+    }
     public String solution(String p) {
-        String answer = "";
-        
-        
-        
-        return answer;
+        if(isCorrect(p))
+            return p;
+        return dfs(p);
     }
 }
+ 
