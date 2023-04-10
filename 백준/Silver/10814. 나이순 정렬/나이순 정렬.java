0f38de428bf79f0edb1c505ff96c397d676f09ad
Limwngur <101847711+Juhyeok0202@users.noreply.github.com>
2023-04-11 05:19:37 +0900

    [Silver V] Title: 나이순 정렬, Time: 1336 ms, Memory: 145236 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,56 +1,41 @@
-import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
- 
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {		
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-        
-		Person[] p = new Person[N];
-		
- 
-		for(int i = 0; i < N; i++) {
-			p[i] = new Person(in.nextInt(), in.next());
-		}
- 
-		
-		// 타입을 Person 으로 둘 것.
-		Arrays.sort(p, new Comparator<Person>() {
-			// 나이순으로 정렬
-			@Override
-			public int compare(Person s1, Person s2) {
-				return s1.age - s2.age;
-			}
-			
-		});
- 
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < N; i++) {
-			// 객체배열의 객체를 출력하면 해당 인덱스의 객체의 toString() 이 출력 됨
-			sb.append(p[i]);
-		}
-        
-		System.out.println(sb);
-		
-	}
-    
-	public static class Person {
-		int age;
-		String name;
-        
-		public Person(int age, String name) {
-			this.age = age;
-			this.name = name;
-		}
-        
-		@Override
-		public String toString() {
-			return age + " " + name + "\n";
-		}
-	}
- 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N;
+        N = in.nextInt();
+
+        Info[] info_p = new Info[N];
+
+        for (int i = 0; i < N; ++i) {
+            info_p[i] = new Info(in.nextInt(), in.next());
+        }
+
+        Arrays.sort(info_p, (a,b) -> a.age - b.age);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; ++i) {
+            sb.append(info_p[i]);
+        }
+        System.out.println(sb);
+    }
+}
+
+class Info {
+    int age;
+    String name;
+
+    public Info(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return age+" "+name+"\n";
+    }
 }
