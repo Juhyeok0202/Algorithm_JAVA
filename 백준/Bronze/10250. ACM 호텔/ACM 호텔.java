05efa90bd3157c1ee4e5c1fb5c3929d0825efcd5
Limwngur <101847711+Juhyeok0202@users.noreply.github.com>
2023-05-07 11:35:22 +0900

    [Bronze III] Title: ACM 호텔, Time: 244 ms, Memory: 18112 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,33 +1,35 @@
 import java.util.Scanner;
 
+/**
+ * 첫번째 풀이가 너무 비효율 적이어서 수학적인 풀이로 재 해석
+ * 첫번째 풀이 방법
+ * 1.반복문을 통한 TestCase번 반복
+ * 2.double-loop 를 통해서 floorNumber-loop와 roomNumber-loop로 하여 결과값을 받음
+ * 따라서, 시간 복잡도가 매우 큼
+ * O(WXHXN)으로 W,H,N 값이 크면 클 수록 시간이 기하적으로 증가할 수 있음.
+ */
 public class   Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int T,H,W,N;
-
+        int floorNumber;
+        int roomNumber;
         T = in.nextInt();   //#OF TESTCASE
 
-        for(int k=0; k<T ;++k){
+        for(int k=0; k<T ;++k) {
             H = in.nextInt();   //height
-            W = in.nextInt();   //width
+            W = in.nextInt();   //width 필요없는 변수
             N = in.nextInt();   //nth customer
 
-
-            int [][]hotel = new int [W][H];
-
-//        50 40 30 20 10 00 51 41 31 21 11 10 52 41~~~5 11 4 11 3 11 2 11 1 11 0 11
-            int count=0;
-            int result=0;
-            for (int j = 1; j <=W; ++j) { // 같은 층 호수
-                for (int i = 1; i <= H; ++i) { //층 위치
-                    count++;
-                    result = i*100 + j;
-                    if (count == N) break;
-                }
-                if(count==N)break;
+            if(N%H==0){
+                floorNumber=H*100;
+                roomNumber =N/H;
+            }else{
+                floorNumber = N%H*100;
+                roomNumber = N/H +1;
             }
-
-            System.out.println(result);
+            int roomCode = floorNumber + roomNumber;
+            System.out.println(roomCode);
         }
         in.close();
 
