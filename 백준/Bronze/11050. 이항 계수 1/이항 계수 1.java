bae114fdfa160088dcc00581bf4c77ec9ae9f737
Limwngur <101847711+Juhyeok0202@users.noreply.github.com>
2023-05-16 20:51:41 +0900

    [Bronze I] Title: 이항 계수 1, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -4,17 +4,27 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class   Main {
+    static int [][]dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+
+        dp=new int [N+1][K+1];
+
         System.out.println(binomialCoefficient(N,K));
     }
 
     static int binomialCoefficient(int N, int K) {
-        if(N==K | K==0) return 1;
+        if (dp[N][K] > 0) { //동적 계획법의 핵심. 이미 존재하는(저장되어있는)것은 넘어간다.
+            return dp[N][K];
+        }
 
-        return binomialCoefficient(N - 1, K - 1) + binomialCoefficient(N - 1, K);
+        if (N == K || K == 0) {
+            return dp[N][K]=1;
+        }
+
+        return dp[N][K] = binomialCoefficient(N - 1, K - 1) + binomialCoefficient(N - 1, K);
     }
 }
